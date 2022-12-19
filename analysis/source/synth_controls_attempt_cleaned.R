6b20e0d285fbfffa22c9ea7d3fefd91b224a87e0
tessabonomo <tbonomo003@gmail.com>
2022-12-19 14:11:10 -0500

    added number of jobs

--- analysis/source/synth_controls_attempt_cleaned.R
+++ analysis/source/synth_controls_attempt_cleaned.R
@@ -40,6 +40,8 @@ cpi <-
          inflator = c(0.741, 0.726, 0.715, 0.704,
                       0.703, 0.694, 0.679, 0.663, 0.652) * 	1.507)
 
+# Define max income for winsorization
+
 inc_max <-
   df_full %>%
   filter(year != 2012,
@@ -47,34 +49,32 @@ inc_max <-
   summarise(wtd.quantile(hhincome, asecwth, 0.975)) %>%
   pull()
 
-ind_desc <-
-  read_csv("~/repo/fair_work/analysis/input/industry.csv")
+# Read in industry and occupation descriptions
 
-ind90_desc <-
-  read_csv("~/repo/fair_work/analysis/input/industry90.csv")
+ind_desc <- read_csv("~/repo/fair_work/analysis/input/industry.csv")
 
-occ_desc <-
-  read_csv("~/repo/fair_work/analysis/input/occupation.csv")
+ind90_desc <- read_csv("~/repo/fair_work/analysis/input/industry90.csv")
+
+occ_desc <- read_csv("~/repo/fair_work/analysis/input/occupation.csv")
 
 #-------------------------------------------------------------------------------
+### ASEC analysis (age, race, sex, income, industry of employment)
 
-# Create necessary variables
-
+# Create necessary variables from ASEC
 df_clean <-
   df_full %>%
-  group_by(year, serial, pernum) %>%
-  mutate(numjob = max(numjob)) %>%
   filter(year != 2012,
          between(age, min_age, max_age),
          asecflag == 1) %>% 
   inner_join(cpi, by = "year") %>% 
-  mutate(hhincome = hhincome*inflator,
+  mutate(hhincome = ifelse(hhincome > 9999000 | hhincome < 0, NA_real_, hhincome),
+         hhincome = hhincome*inflator,
          hhincome = ifelse(hhincome > inc_max, inc_max, hhincome),
-         unemp = case_when(empstat %in% c(10, 12) ~ 0,
-                         empstat %in% c(20, 21, 22) ~ 1,
-                         T ~ NA_real_),
-         in_affected_ind = case_when(unemp == 0 & ind90ly %in% ind90_list ~ 1,
-                                     unemp == 0 ~ 0,
+         unemp_asec = case_when(workly == 2 ~ 0,
+                                wantjob == 0 ~ 1,
+                                T ~ NA_real_),
+         in_affected_ind = case_when(unemp_asec == 0 & ind90ly %in% ind90_list ~ 1,
+                                     unemp_asec == 0 ~ 0,
                                      T ~ NA_real_),
          black = race == 200,
          white = race == 100,
@@ -91,16 +91,14 @@ df_clean <-
          metarea = statefip*10000 + metarea,
          effective_year = year - 1,
          ln_income = log(hhincome + 0.001),
-         owns_home = ownershp == 10) 
+         owns_home = ownershp == 10,
+         cpsidp = ifelse(cpsidp == 0, NA_real_, cpsidp)) 
 
-#-------------------------------------------------------------------------------
+# Separate dataframe of just affected industry workers (ASEC)
 
-# Separate dataframe of just affected industry workers
-
-df_industry_workers <-
+df_industry_workers_asec <-
   df_clean %>%
-  filter(hhincome < 9999999 * inflator,
-         hhincome >= 0,
+  filter(!is.na(hhincome),
          ind90ly %in% ind90_list) %>%
   left_join(occ_desc) %>%
   filter(!str_detect(occ_desc, "manager")) %>%
@@ -108,58 +106,109 @@ df_industry_workers <-
   filter(pernum == min(pernum)) %>%
   ungroup() 
 
-#-------------------------------------------------------------------------------
-
-# Aggregate individual-level predictors (race, age, sex, 
+# Aggregate individual-level predictors from ASEC (race, age, sex, 
 # unemployment, employment in affected industries)
 
-df_agg_ind <-
+df_agg_ind_asec <-
   df_clean %>%
   group_by(statefip, effective_year) %>%
-  summarise(across(c("unemp", "in_affected_ind", #"single", 
-                     "black", "white", "hispan", "asian", "age", "female"), 
+  summarise(across(c("in_affected_ind","black", "white", "hispan",
+                     "asian", "age", "female"), 
                    ~ weighted.mean(.x, asecwt, na.rm = T)))
 
 # Aggregate household-level predictors (race, age, sex, 
 # unemployment, employment in affected industries)
 
-df_agg_hh <-
+df_agg_hh_asec <-
   df_clean %>%
   filter(pernum == 1,
-         hhincome < 9999999 * inflator, #should I winsorize income?
-         hhincome >= 0) %>%
+         !is.na(hhincome)) %>%
   group_by(statefip, effective_year) %>%
-  summarise(across(c("ln_income", "owns_home", "famsize"), 
-                   ~ weighted.mean(.x, asecwth, na.rm = T)),
-            n_hh = n())
+  summarise(across(c("ln_income"), 
+                   ~ weighted.mean(.x, asecwth)))
 
 # Aggregate household-level predictors for workers in affected industries
 # (log income)
 
-df_agg_hh_affected <-
-  df_industry_workers %>%
+df_agg_hh_affected_asec <-
+  df_industry_workers_asec %>%
   group_by(statefip, effective_year) %>%
-  summarise(weight = sum(asecwth),
-            ln_income_industry = weighted.mean(ln_income, asecwth))
+  summarise(ln_income_industry = weighted.mean(ln_income, asecwth))
+
+#-------------------------------------------------------------------------------
+### March Basic aggregation (num jobs, unemployment rates)
+
+# Create necessary variables from March Basic
+
+df_march_basic <-
+  df_full %>%
+  filter(year != 2020,
+         between(age, min_age, max_age),
+         asecflag == 2) %>% 
+  transmute(unemp = case_when(empstat %in% c(10, 12) ~ 0,
+                              empstat %in% c(20, 21, 22) ~ 1,
+                              T ~ NA_real_),
+            numjob = case_when(multjob == 1 ~ 1,
+                               multjob == 2 & numjob == 2 ~ 2,
+                               multjob == 2 & numjob == 3 ~ 3,
+                               multjob == 2 & numjob == 4 ~ 4,
+                               T ~ NA_real_),
+            metarea = statefip*10000 + metarea,
+            effective_year = year,
+            statefip,
+            wtfinl,
+            cpsidp,
+            ind1990,
+            occ,
+            serial,
+            pernum) 
 
 
+df_industry_workers_mb <-
+  df_march_basic %>%
+  filter(ind1990 %in% ind90_list) %>%
+  left_join(occ_desc %>% rename(occ = "occly")) %>%
+  filter(!str_detect(occ_desc, "manager")) %>%
+  group_by(serial) %>%
+  filter(pernum == min(pernum)) %>%
+  ungroup() 
+
+# Aggregate individual-level predictors from March Basic (unemployment)
+
+df_agg_ind_mb <-
+  df_march_basic %>%
+  group_by(statefip, effective_year) %>%
+  summarise(across(c("unemp"), 
+                   ~ weighted.mean(.x, wtfinl, na.rm = T)))
+
+
+df_agg_ind_affected_mb <-
+  df_industry_workers_mb %>%
+  filter(unemp == 0) %>%
+  group_by(statefip, effective_year) %>%
+  summarise(numjobs = weighted.mean(numjob, wtfinl),
+            more_than_one_job = weighted.mean(numjob > 1, wtfinl))
+
+#-------------------------------------------------------------------------------
 
 df_agg <-
-  df_agg_hh %>%
-  inner_join(df_agg_ind, by = c("statefip", "effective_year"))%>%
-  inner_join(df_agg_hh_affected, by = c("statefip", "effective_year"))%>%
+  df_agg_ind_asec %>%
+  inner_join(df_agg_hh_asec, by = c("statefip", "effective_year"))%>%
+  inner_join(df_agg_ind_mb, by = c("statefip", "effective_year"))%>%
+  inner_join(df_agg_hh_affected_asec, by = c("statefip", "effective_year"))%>%
+  inner_join(df_agg_ind_affected_mb, by = c("statefip", "effective_year"))%>%
   filter(!is.na(unemp),
-         !is.na(ln_income_industry),
-         n_hh >= 20) %>%
+         !is.na(ln_income_industry)) %>%
   mutate(statefip = as.character(statefip)) %>%
   group_by(statefip) %>%
-  filter(n() == n_years) #make panel
+  filter(n() == n_years) %>%
+  filter(!(statefip %in% c(36, 6, 53, 42, 17)))
 
 
 #-------------------------------------------------------------------------------
 
 industries <- 
-  df_industry_workers %>% 
+  df_industry_workers_asec %>% 
   group_by(indly, ind90ly) %>%
   summarise(n = n(),
             share_hourly = weighted.mean(hourly_worker, asecwt, na.rm = T),
@@ -170,7 +219,7 @@ industries <-
   left_join(ind90_desc)
 
 occupations <- 
-  df_industry_workers %>% 
+  df_industry_workers_asec %>% 
   group_by(occly) %>%
   summarise(n = n()) %>%
   left_join(occ_desc)
@@ -180,15 +229,44 @@ occupations <-
 #-------------------------------------------------------------------------------
 
 # Create list of predictor variables
-predictor_list <- c("ln_income", "unemp", "in_affected_ind", "black", 
-                    "white", "hispan", "asian", "age", "female") #add education
+predictor_list <- c("ln_income", "unemp", "in_affected_ind") #add education
+# predictor_list <- c("ln_income", "unemp", "in_affected_ind", "age", "female") #add education
 
-
-# Create list of metro areas (if doing metro analysis)
-# metro_list <- c("41900", "412400", "414890", "416442", "417080", "419998", "419999")
 
 # Function that creates synthetic control
-synth_func <- function(state, predictors){
+synth_func_numjobs <- function(state, predictors){
+  
+  output <-
+    df_agg %>%
+    synthetic_control(outcome = more_than_one_job, # outcome
+                      unit = statefip, # unit index in the panel data
+                      time = effective_year, # time index in the panel data
+                      i_unit = state, # unit where the intervention occurred
+                      i_time = 2017, # time period when the intervention occurred
+                      generate_placebos=T) %>%
+    generate_predictor(time_window = 2012:2016,
+                       across(predictors, ~ mean(.x, na.rm = T))) %>%
+    generate_predictor(time_window = 2012,
+                       numjobs_2012 = more_than_one_job) %>%
+    generate_predictor(time_window = 2013,
+                       numjobs_2013 = more_than_one_job) %>%
+    generate_predictor(time_window = 2014,
+                       numjobs_2014 = more_than_one_job) %>%
+    generate_predictor(time_window = 2015,
+                       numjobs_2015 = more_than_one_job) %>%
+    generate_predictor(time_window = 2016,
+                       numjobs_2016 = more_than_one_job) %>%
+    generate_weights(optimization_window = 2012:2016, # time to use in the optimization task
+                     margin_ipop = .02,sigf_ipop = 7,bound_ipop = 6 # optimizer options
+    ) %>%
+    generate_control()
+  
+  output
+}
+
+
+# Function that creates synthetic control
+synth_func_income<- function(state, predictors){
   
   output <-
     df_agg %>%
@@ -218,17 +296,18 @@ synth_func <- function(state, predictors){
   output
 }
 
-# Create synthetic Oregon
-synthetic_oregon <- 
-  synth_func("41", predictor_list)
+# Create synthetic Oregon Income
+
+synthetic_oregon_income <- 
+  synth_func_income("41", predictor_list)
 
 # Create balance table
 balance_table <-
-  synthetic_oregon %>%
+  synthetic_oregon_income %>%
   grab_balance_table()
 
 # Plot trends
-synthetic_oregon %>% 
+synthetic_oregon_income %>% 
   plot_trends() +
   ylab("Log income in affected industries") +
   xlab("Year") +
@@ -236,28 +315,113 @@ synthetic_oregon %>%
   scale_color_manual(values = c("#b41e7c", "grey60"),  labels=c('Real Oregon', 'Synthetic Oregon'))+
   scale_linetype_manual(values = c(1, 4),  labels=c('Real Oregon', 'Synthetic Oregon'))
 
-# Plot differences
-synthetic_oregon %>%
-  plot_differences() +
-  ylab("Change in log income in affected industries") +
-  xlab("Year") +
-  ggtitle("Change in log income in affected industries") 
-
 # Plot placebos
-synthetic_oregon %>%
-  plot_placebos() +
+synthetic_oregon_income %>%
+  plot_placebos_5() +
   ylab("Change in log income in affected industries") +
   xlab("Year") +
   ggtitle("Change in log income in affected industries") +
   scale_color_manual(values = c("#b41e7c", "grey60"),  labels=c('Oregon', 'Placebos'))+
   scale_alpha_manual(values = c(1, 0.4),  labels=c('Oregon', 'Placebos')) +
   scale_size_manual(values = c(1, 0.5),  labels=c('Oregon', 'Placebos'))
-  
+
 # Create weight table
-weights_table <- 
-  synthetic_oregon %>%
+weights_table_inc <- 
+  synthetic_oregon_income %>%
   grab_unit_weights() %>%
   filter(weight > 0.01)
 
-synthetic_oregon %>%
+synthetic_oregon_income %>%
   grab_predictor_weights()
+
+#Inference
+t <-
+  synthetic_oregon_income %>% grab_signficance() 
+
+ggplot(t, aes(y = mspe_ratio))+
+  geom_histogram() +
+  geom_hline(yintercept = 11.6816117)
+
+# Create synthetic Oregon Num jobs 
+
+synthetic_oregon_numjobs <- 
+  synth_func_numjobs("41", predictor_list)
+
+# Plot trends
+synthetic_oregon_numjobs %>% 
+  plot_trends() +
+  ylab("Average number of jobs held in affected industries") +
+  xlab("Year") +
+  ggtitle("Average number of jobs held in affected industries") +
+  scale_color_manual(values = c("#b41e7c", "grey60"),  labels=c('Real Oregon', 'Synthetic Oregon'))+
+  scale_linetype_manual(values = c(1, 4),  labels=c('Real Oregon', 'Synthetic Oregon'))
+
+# Plot placebos
+synthetic_oregon_numjobs %>%
+  plot_placebos_5(prune = T) +
+  ylab("Change in number of jobs") +
+  xlab("Year") +
+  ggtitle("Change in number of jobs in affected industries") +
+  scale_color_manual(values = c("#b41e7c", "grey60"),  labels=c('Oregon', 'Placebos'))+
+  scale_alpha_manual(values = c(1, 0.4),  labels=c('Oregon', 'Placebos')) +
+  scale_size_manual(values = c(1, 0.5),  labels=c('Oregon', 'Placebos'))
+
+# Create weight table
+weights_table_nj <- 
+  synthetic_oregon_numjobs %>%
+  grab_unit_weights() %>%
+  filter(weight > 0.01)
+
+synthetic_oregon_numjobs %>%
+  grab_predictor_weights()
+
+synthetic_oregon_numjobs %>% plot_mspe_ratio()
+
+#Inference
+t <-
+  synthetic_oregon_numjobs %>% grab_signficance() 
+
+ggplot(t, aes(y = mspe_ratio))+
+  geom_histogram() +
+  geom_hline(yintercept = 13.8335183)
+
+plot_placebos_5 <- function (data, time_window = NULL, prune = TRUE) 
+{
+  if (!(".meta" %in% colnames(data))) {
+    stop("`.meta` column has been removed. `.meta` column needs to be included for `generte_control()` to work.")
+  }
+  trt_time <- data$.meta[[1]]$treatment_time[1]
+  time_index <- data$.meta[[1]]$time_index[1]
+  treatment_unit <- data$.meta[[1]]$treatment_unit[1]
+  unit_index <- data$.meta[[1]]$unit_index[1]
+  outcome_name <- data$.meta[[1]]$outcome[1]
+  if (is.null(time_window)) {
+    time_window <- unique(data$.original_data[[1]][[time_index]])
+  }
+  plot_data <- data %>% grab_synthetic_control(placebo = TRUE) %>% 
+    dplyr::mutate(diff = real_y - synth_y) %>% dplyr::filter(time_unit %in% 
+                                                               time_window) %>% dplyr::mutate(type_text = ifelse(.placebo == 
+                                                                                                                   0, treatment_unit, "control units"), type_text = factor(type_text, 
+                                                                                                                                                                           levels = c(treatment_unit, "control units")))
+  caption <- ""
+  if (prune) {
+    sig_data = data %>% grab_signficance(time_window = time_window)
+    thres <- sig_data %>% dplyr::filter(type == "Treated") %>% 
+      dplyr::pull(pre_mspe) %>% sqrt(.)
+    retain_ <- sig_data %>% dplyr::select(unit_name, pre_mspe) %>% 
+      dplyr::filter(sqrt(pre_mspe) <= thres * 5) %>% dplyr::pull(unit_name)
+    plot_data <- plot_data %>% dplyr::filter(.id %in% retain_)
+    caption <- "Pruned all placebo cases with a pre-period RMSPE exceeding two times the treated unit's pre-period RMSPE."
+  }
+  plot_data %>% ggplot2::ggplot(ggplot2::aes(time_unit, diff, 
+                                             group = .id, color = type_text, alpha = type_text, size = type_text)) + 
+    ggplot2::geom_hline(yintercept = 0, color = "black", 
+                        linetype = 2) + ggplot2::geom_vline(xintercept = trt_time, 
+                                                            color = "black", linetype = 3) + ggplot2::geom_line() + 
+    ggplot2::scale_color_manual(values = c("#b41e7c", "grey60")) + 
+    ggplot2::scale_alpha_manual(values = c(1, 0.4)) + ggplot2::scale_size_manual(values = c(1, 
+                                                                                            0.5)) + ggplot2::labs(color = "", alpha = "", size = "", 
+                                                                                                                  y = outcome_name, x = time_index, title = paste0("Difference of each '", 
+                                                                                                                                                                   unit_index, "' in the donor pool"), caption = caption) + 
+    ggplot2::theme_minimal() + ggplot2::theme(legend.position = "bottom")
+}
